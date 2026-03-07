c3e70e2fcd81a2f7231066a01009f696799199f1
Ahmed Ali Al Mutawakel <202310101668@student.ust.edu.ye>
2026-03-07 07:04:01 +0300

    Configure Maven publishing to GitHub Packages
    Add publishing configuration for Maven to GitHub Packages.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,30 @@ plugins {
     id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
     id("maven-publish")
 }
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                // تأكد من وضع اسم حسابك الصحيح في جيت هاب
+                groupId = "com.github.ahmedaliahmed775" 
+                artifactId = "atheer-sdk"
+                version = "1.0.0"
+
+                afterEvaluate {
+                    from(components["release"])
+                }
+            }
+        }
+        
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/ahmedaliahmed775/atheer-sdk")
+                credentials {
+                    username = System.getenv("GITHUB_ACTOR")
+                    password = System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
+    }
+}
