7b05ee4f0629df93be264023294b2972caf1cdf0
Ahmed Ali Al Mutawakel <202310101668@student.ust.edu.ye>
2026-03-07 07:56:40 +0300

    Remove maven-publish plugin and related settings
    Removed maven-publish plugin and its configuration for GitHub Packages.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,32 +3,4 @@ plugins {
     id("com.android.library") version "8.3.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
-    id("maven-publish")
-}
-afterEvaluate {
-    publishing {
-        publications {
-            register<MavenPublication>("release") {
-                // تأكد من وضع اسم حسابك الصحيح في جيت هاب
-                groupId = "com.github.ahmedaliahmed775" 
-                artifactId = "atheer-sdk"
-                version = "1.0.0"
-
-                afterEvaluate {
-                    from(components["release"])
-                }
-            }
-        }
-        
-        repositories {
-            maven {
-                name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/ahmedaliahmed775/atheer-sdk")
-                credentials {
-                    username = System.getenv("GITHUB_ACTOR")
-                    password = System.getenv("GITHUB_TOKEN")
-                }
-            }
-        }
-    }
 }
