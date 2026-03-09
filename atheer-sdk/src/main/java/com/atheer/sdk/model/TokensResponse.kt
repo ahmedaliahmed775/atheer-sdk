fa20f19c187d716af101f6436afc221ef5b1acea
Ahmed Ali Al Mutawakel <202310101668@student.ust.edu.ye>
2026-03-10 00:12:57 +0300

    Add TokensResponse and TokensData data classes

---
+++ atheer-sdk/src/main/java/com/atheer/sdk/model/TokensResponse.kt
@@ -0,0 +1,12 @@
+package com.atheer.sdk.model
+
+import com.google.gson.annotations.SerializedName
+
+data class TokensResponse(
+    @SerializedName("success") val success: Boolean,
+    @SerializedName("data") val data: TokensData?
+)
+
+data class TokensData(
+    @SerializedName("tokens") val tokens: List<String>
+)
