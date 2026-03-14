27398fa5d5b1b8b55e649fc608c4fdeece7eb506
Ahmed Ali Al Mutawakel <202310101668@student.ust.edu.ye>
2026-03-14 23:38:03 +0300

    Add message field to TokensResponse data class

--- atheer-sdk/src/main/java/com/atheer/sdk/model/TokensResponse.kt
+++ atheer-sdk/src/main/java/com/atheer/sdk/model/TokensResponse.kt
@@ -4,6 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class TokensResponse(
     @SerializedName("success") val success: Boolean,
+    @SerializedName("message") val message: String?,
     @SerializedName("data") val data: TokensData?
 )
 
